507940033d4c71e05fc53ec81887c7a81d6a607d
ibrahimpl <89786754+ibrahimpl@users.noreply.github.com>
2023-06-20 11:38:34 -0400

    Removing the example

--- example/lib/pass_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/services.dart';
-
-const SAMPLE_PASS_PATH = 'assets/passes/health_id_card_sample.pkpass';
-
-final Future<Uint8List> Function() passProvider = () async {
-  ByteData pass = await rootBundle.load(SAMPLE_PASS_PATH);
-  return pass.buffer.asUint8List();
-};
